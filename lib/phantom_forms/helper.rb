76eb4b00cb98f787c6ed773458d73d0f3c17297d
snowsunny <snowsunny1107@gmail.com>
2013-09-18 13:37:51 +0900

    added remote_form_panel_for

--- lib/phantom_forms/helper.rb
+++ lib/phantom_forms/helper.rb
@@ -1,25 +1,37 @@
 module PhantomForms
   module Helper
 
-    def remote_form_for(object, options = {}, panel_title = nil, &block)
+    def remote_form_for(object, options = {}, &block)
       options[:validate] = true
       options[:builder] = PhantomForms::FormBuilders::ValidateFormBuilder
       options[:remote] = true
       content_tag :div, class: "col-md-12" do
-        content_tag :div, class: "panel panel-primary" do
-          [
-            if panel_title
-              content_tag :div, class: "panel-heading" do
-                content_tag :h3, class: "panel-title" do
-                  panel_title
-                end
-              end
-            end,
-            content_tag(:div, class: "panel-body") do
-              form_for(object, options, &block)
-            end
-          ].join.html_safe
+        content_tag :div, class: "well" do
+          form_for(object, options, &block)
         end
+      end
+    end
+
+    def remote_form_panel_for(object, panel_title = nil, options = {}, &block)
+      options[:validate] = true
+      options[:builder] = PhantomForms::FormBuilders::ValidateFormBuilder
+      options[:remote] = true
+      content_tag :div, class: "panel panel-primary" do
+        [
+          if panel_title
+            content_tag :div, class: "panel-heading" do
+              content_tag :h3, class: "panel-title" do
+                panel_title
+              end
+            end
+          end,
+          content_tag(:div, class: "panel-body") do
+            form_for(object, options, &block)
+          end,
+          content_tag(:div, class: "panel-footer") do
+            buttons_for object
+          end
+        ].join.html_safe
       end
     end
 
@@ -58,10 +70,10 @@ module PhantomForms
 
       content_tag :div, :class => 'row' do
         [
-          content_tag(:div, :class => 'col-md-6') do
+          content_tag(:div, :class => 'col-md-9') do
             concat submit_button( locale , :id => "submit-#{object_class}-button")
           end,
-          content_tag(:div, :class => 'col-md-6') do
+          content_tag(:div, :class => 'col-md-3') do
             concat link_to_cancel( :id => "cancel-#{object_class}-link")
           end
         ].join.html_safe
